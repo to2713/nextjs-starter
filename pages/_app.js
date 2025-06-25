479ba1d2b9813fe78be131ebab0db2c7cd31365a
to2713 <v-tkawabata@microsoft.com>
2025-06-25 17:33:12 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/reset.css';
 import '../styles/main.css';
 
-import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
-const appInsights = new ApplicationInsights({ config: {
-  connectionString: 'InstrumentationKey=ac12c1b6-04e0-4896-9903-28da40444b40;IngestionEndpoint=https://japaneast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://japaneast.livediagnostics.monitor.azure.com/;ApplicationId=52cbf1aa-aacd-4a39-9888-f68fe5781311'
-  /* ...Other Configuration Options... */
-} });
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString:
+      'InstrumentationKey=ac12c1b6-04e0-4896-9903-28da40444b40;IngestionEndpoint=https://japaneast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://japaneast.livediagnostics.monitor.azure.com/;ApplicationId=52cbf1aa-aacd-4a39-9888-f68fe5781311',
+    // ...Other Configuration Options...
+  },
+});
 appInsights.loadAppInsights();
 appInsights.trackPageView();
 
